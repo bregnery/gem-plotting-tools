ff37d476e97699cfe99d8962e96bf3835f8b9101
Brendan <bjregnery@ucdavis.edu>
2019-11-07 15:54:54 +0100

    Deleted a comment and whitespace

--- utils/threshAlgos.py
+++ utils/threshAlgos.py
@@ -1508,14 +1508,13 @@ def sbitRateAnalysis(chamber_config, rateTree, cutOffRate=0.0, debug=False, outf
                     if type(graph) == r.TH1F :
                         graph = r.TGraph(graph)
 
-                    # make sure the yaxis range is restricted
                     graph.GetYaxis().SetRangeUser(1e-1,1e8)
 
                     # Draw a line on the graphs
                     kneeLine.append(r.TLine(dict_dacInflectPts[dacName][ohKey][vfat][0], 1e-1, dict_dacInflectPts[dacName][ohKey][vfat][0], 1e8) )
                     kneeLine[vfat].SetLineColor(2)
                     kneeLine[vfat].SetVertical()
-                    canv_Summary1D.cd(chamber_vfatPos2PadIdx[gemType][vfat] )   
+                    canv_Summary1D.cd(chamber_vfatPos2PadIdx[gemType][vfat] )
                     kneeLine[vfat].Draw()
                 canv_Summary1D.Update()
 
